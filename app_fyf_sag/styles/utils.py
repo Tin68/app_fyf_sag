e29d223f535cf21ffbd61c42847729a9a7df6be9
Tin68 <1968.tin@gmail.com>
2025-05-09 23:38:34 +0200

    modificado utils

--- app_fyf_sag/styles/utils.py
+++ app_fyf_sag/styles/utils.py
@@ -19,4 +19,4 @@ login_description = "Por favor autentificarse"
 
 login_algorithm = "HS256"
 login_access_token_duration = 1
-login_secret = "de6de62abecd528273d4bedaeddc5ad95423fee6a37a7fbfe8d50ba49179533b"
+login_secret = "de6de62abecd528273d4bedaeddc5ad95423fee6a37a7fbfe8d50ba49179533b0"
